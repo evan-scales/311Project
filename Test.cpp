91154468ff2e8e81c6a6599d4f8ab7722e5b63b6
Evan <evan@Evans-MacBook-Pro-3.local>
2022-11-28 22:32:16 -0500

    helpful comments

--- Test.cpp
+++ Test.cpp
@@ -14,6 +14,33 @@
 
 using std::cin, std::cout, std::endl;
 
+/**
+ * @brief How to go about creating tests to compare our multi-threaded data structures to the single-threaded ones
+ * 
+ * opsStruct, holds char op int key string value, if no value then value is empty string, string result
+ * 
+ * Generate commands, creatting a new opsStruct for each command
+ * Use unordered_map to get the expected result for the opsStruct
+ * 
+ * vector<opsStruct> commands
+ * int i = 0
+ * unoraered_map<int, string> map
+ * while (i++ < commands.size()) 
+ *    commands[i].result = map.runOp(commands[i])
+ * 
+ * No runOp command on unordered_map, so use switch statement to get op
+ * then insert, delete, or find the key
+ * Insert: "OK" if inserted, "FAIL" if not inserted
+ * Delete: "OK" if deleted, "FAIL" if not deleted
+ * Lookup: "{value}" if found, "No {key}" if not found
+ * 
+ * Write the commands to a file
+ * Write the expected results to a file
+ * Write a function to compare 2 result output files to see if they match
+ * 
+ */
+
 int main() {
     cout << "Hey, there's nothing here yet!" << endl;
+    return 0;
 }
